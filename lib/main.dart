3d2030150ff9c9e5edb6c9bef05ac8b2277d3026
Vladimir Belau <byelau@mail.com>
2019-10-07 06:09:18 +0200

    delete counter

--- lib/main.dart
+++ lib/main.dart
@@ -154,17 +154,6 @@ class _MyHomePageState extends State<MyHomePage> {
 //return TimeOfDay.now();
   }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -206,17 +195,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 style: Theme.of(context).textTheme.display1,
               ),
             ),
-            /*Text(
-              '$_counter',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .display1,
-            ),*/
-/*            Text(
-              "Kommen: " + '$_start.hour' + ':' + '$_start.minute',
-              style: Theme.of(context).textTheme.display1,
-            ),*/
+
             _TimePicker(
               labelText: 'From',
               selectedTime: _start,
@@ -243,11 +222,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      /*floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.*/
     );
   }
 }
